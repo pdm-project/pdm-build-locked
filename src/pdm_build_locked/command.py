f063b1d9736747ee3302933540421a2ece6be6fd
sigma67 <sigma67.github@gmail.com>
2024-07-21 16:08:26 +0200

    cross-python fix for pdm 2.17

--- src/pdm_build_locked/command.py
+++ src/pdm_build_locked/command.py
@@ -7,6 +7,7 @@ import inspect
 import os
 import subprocess
 from contextlib import suppress
+from importlib import import_module
 from typing import Dict, List, Optional, Tuple, Union
 
 from pdm.cli import actions
@@ -139,14 +140,19 @@ class BuildCommand(BaseCommand):
         from pdm.cli.actions import resolve_candidates_from_lockfile
 
         supported_params = inspect.signature(resolve_candidates_from_lockfile).parameters
-        if "cross_platform" in supported_params:
+        if "env_spec" in supported_params:
+            # pdm 2.17.0+
+            requirements = list(project.get_dependencies(group))
+            markers = import_module("pdm.models.markers")
+            # use lowest supported specifier to include all deps - we don't know the target Python at build time
+            env_spec = markers.EnvSpec.from_spec(">=3.8")
+            candidates = resolve_candidates_from_lockfile(
+                project, requirements, cross_platform=True, groups=[group], env_spec=env_spec
+            )
+        elif "cross_platform" in supported_params:
             # pdm 2.11.0+
             requirements = list(project.get_dependencies(group).values())  # type: ignore[attr-defined]
             candidates = resolve_candidates_from_lockfile(project, requirements, cross_platform=True, groups=[group])
-        elif "env_spec" in supported_params:
-            # pdm 2.17.0+
-            requirements = list(project.get_dependencies(group))
-            candidates = resolve_candidates_from_lockfile(project, requirements, groups=[group])
         else:
             raise PdmException("Unsupported pdm version. pdm>=2.11 is required")
 
