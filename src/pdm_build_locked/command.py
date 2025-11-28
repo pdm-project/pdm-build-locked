e420cae93bef4dc2073c70aeb1c079ba5106b4eb
sigma67 <16363825+sigma67@users.noreply.github.com>
2025-11-28 17:21:24 +0100

    fix: ensure compatibility of pdm build --locked with pdm 2.26.2 (fixes RuntimeError: TOMLFile not opened for write) (#30)
    * bump pdm to 2.26.2
    
    * try fix bug
    
    * revert: check if 3.10 failure exists earlier than 26.2
    
    * Revert "revert: check if 3.10 failure exists earlier than 26.2"
    
    This reverts commit b4e517f309476185e2f6b0f6565fb2bbbace56c2.
    
    * fix the fix

--- src/pdm_build_locked/command.py
+++ src/pdm_build_locked/command.py
@@ -44,6 +44,9 @@ class BuildCommand(BaseCommand):
             super().handle(project, options)
             return
 
+        # we need to let pdm known that we're intending to write this file
+        project.pyproject.open_for_write()
+
         # we are not interested in the pdm dev-dependencies group
         pdm_dev_dependencies = set()
         if dev_dependencies := project.pyproject.settings.get("dev-dependencies"):
